bb6497db772db4c1475e9055c7b28999c1952a70
Usman <ui.sabuwala@gmail.com>
2022-07-01 15:15:13 +0530

    Used Next.js Script tag for analyics

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,19 +1,18 @@
 import { Container } from '@chakra-ui/react';
 import Head from 'next/head';
+import Script from 'next/script';
 import { ReactNode } from 'react';
 import { Navbar } from './Navbar';
 
 export const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <>
-      <Head>
-        <script
-          async
-          defer
-          data-website-id='4b493259-b01a-4728-976a-c357291ec12e'
-          src='https://analytics.usman-s.me/umami.js'
-        ></script>
-      </Head>
+      <Script
+        async
+        defer
+        data-website-id='4b493259-b01a-4728-976a-c357291ec12e'
+        src='https://analytics.usman-s.me/umami.js'
+      />
       <Navbar />
       <Container my='4' maxW='container.lg'>
         {children}
