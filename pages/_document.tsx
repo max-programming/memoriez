4c1c46d2870d5a8db777fe9a0895e0bc9daa3127
Usman <ui.sabuwala@gmail.com>
2022-07-01 15:52:29 +0530

    Change _document to function

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,24 +1,20 @@
 import { Head, Html, Main, NextScript } from 'next/document';
 
-class Document {
-  render() {
-    return (
-      <Html lang='en'>
-        <Head>
-          <script
-            async
-            defer
-            data-website-id='4b493259-b01a-4728-976a-c357291ec12e'
-            src='https://analytics.usman-s.me/umami.js'
-          ></script>
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <script
+          async
+          defer
+          data-website-id='4b493259-b01a-4728-976a-c357291ec12e'
+          src='https://analytics.usman-s.me/umami.js'
+        ></script>
+      </Head>{' '}
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
-
-export default Document;
