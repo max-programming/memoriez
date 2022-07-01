d26de2fe4996e74fc99972a2297d89647c82c9b9
Usman <ui.sabuwala@gmail.com>
2022-07-01 09:15:14 +0530

    Added analytics

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,10 +1,19 @@
 import { Container } from '@chakra-ui/react';
+import Head from 'next/head';
 import { ReactNode } from 'react';
 import { Navbar } from './Navbar';
 
 export const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <>
+      <Head>
+        <script
+          async
+          defer
+          data-website-id='4b493259-b01a-4728-976a-c357291ec12e'
+          src='https://analytics.usman-s.me/umami.js'
+        ></script>
+      </Head>
       <Navbar />
       <Container my='4' maxW='container.lg'>
         {children}
