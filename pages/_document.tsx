84adbb30e13376a3eb48d01a71e14f7b4018a2cb
Usman Sabuwala (Max Programming) <51731966+max-programming@users.noreply.github.com>
2022-07-02 20:30:56 +0530

    Create _document.tsx

---
+++ pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <script async defer data-website-id="4b493259-b01a-4728-976a-c357291ec12e" src="https://analytics.usman-s.me/umami.js"></script>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
